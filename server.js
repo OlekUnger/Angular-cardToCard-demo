1fbe9652e3e3f90eb927b7ff00987aacd53bdca2
olekunger <ounger@inbox.ru>
2019-07-03 19:36:37 +0300

    -

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist/ungers-card-to-card-demo'));
 app.get('/*', (req,res)=>{
-  res.sendFile(path.join(__dirname, '/dist/ungers-card-to-card-demo/index.html'))
+  res.sendFile(path.join(__dirname, 'dist/ungers-cardToCard-demo/index.html'))
 })
 app.listen(process.env.PORT || 8080);
